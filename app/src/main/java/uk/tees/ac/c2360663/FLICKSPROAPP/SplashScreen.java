790089112bc61a43f4b7a1e3a4ea5a91ce1948e1
Reginald077 <124705078+Reginald077@users.noreply.github.com>
2023-02-21 00:25:53 +0000

    Splash Screen Added

---
+++ app/src/main/java/uk/tees/ac/c2360663/FLICKSPROAPP/SplashScreen.java
@@ -0,0 +1,14 @@
+package uk.tees.ac.c2360663.FLICKSPROAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
