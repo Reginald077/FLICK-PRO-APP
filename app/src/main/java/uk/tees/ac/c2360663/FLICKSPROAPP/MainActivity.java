4235871436eb100943fef88d86b030b9c100caea
Reginald077 <124705078+Reginald077@users.noreply.github.com>
2023-02-24 16:40:10 +0000

    fixed bug on login screen

--- app/src/main/java/uk/tees/ac/c2360663/FLICKSPROAPP/MainActivity.java
+++ app/src/main/java/uk/tees/ac/c2360663/FLICKSPROAPP/MainActivity.java
@@ -2,13 +2,37 @@ package uk.tees.ac.c2360663.FLICKSPROAPP;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button signUpButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        signUpButton = (Button) findViewById(R.id.main_sign_up_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
+
+
+
+
