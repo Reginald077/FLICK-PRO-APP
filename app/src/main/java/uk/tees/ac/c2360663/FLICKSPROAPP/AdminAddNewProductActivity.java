cfe634963204004c32fd5853972ab95cb34a22a7
Reginald077 <124705078+Reginald077@users.noreply.github.com>
2023-03-22 22:46:27 +0000

    Admin Panel Created, Admin category activity

--- app/src/main/java/uk/tees/ac/c2360663/FLICKSPROAPP/AdminAddNewProductActivity.java
+++ app/src/main/java/uk/tees/ac/c2360663/FLICKSPROAPP/AdminAddNewProductActivity.java
@@ -3,10 +3,17 @@ package uk.tees.ac.c2360663.FLICKSPROAPP;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity
 {
+    private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +22,13 @@ public class AdminAddNewProductActivity extends AppCompatActivity
         setContentView(R.layout.activity_admin_add_new_product);
 
 
-        Toast.makeText(this, "Welcome Admin....", Toast.LENGTH_SHORT).show();
+        CategoryName = getIntent().getExtras().get("category").toString();
+
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductDescription = (EditText) findViewById(R.id.product_description);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
+
     }
 }
