cf9e1ff0db85bc6cfdbb6b4a854bac189fc60f1d
Reginald077 <124705078+Reginald077@users.noreply.github.com>
2023-04-29 08:50:26 +0100

    in this activity,i will allow user to edit or remove cart items

---
+++ app/src/main/java/ViewHolder/CartViewHolder.java
@@ -0,0 +1,37 @@
+package ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import Interface.ItemClickListener;
+import uk.tees.ac.c2360663.FLICKSPROAPP.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductPrice, txtProductQuantity;
+    private ItemClickListener itemClickListener;
+
+    public CartViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        itemClickListener.onClick(view, getAdapterPosition(), false);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener)
+    {
+        this.itemClickListener = itemClickListener;
+
+    }
+}
